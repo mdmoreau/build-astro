c7a9957061088e390fe966256e2c69b3ba8a6fe0
mdmoreau <mdmoreau@gmail.com>
2023-05-28 18:11:36 -0400

    refactor css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,17 +1,10 @@
-const globalData = require('@csstools/postcss-global-data');
 const presetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    globalData({
-      files: [
-        'src/styles/media.css',
-      ],
-    }),
     presetEnv({
       stage: false,
       features: {
-        'custom-media-queries': true,
         'media-query-ranges': true,
         'nesting-rules': true,
       },
